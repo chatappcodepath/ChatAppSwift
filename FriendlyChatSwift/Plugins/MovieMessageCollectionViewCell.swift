ef5d5945c1f51a4cd0f81e075adc9af238d6a70c
Kevin Patel <patelkev@yahoo-inc.com>
2016-12-04 20:43:18 -0800

    TapGestureRecognizer is working now for Movie Cell

--- FriendlyChatSwift/Plugins/MovieMessageCollectionViewCell.swift
+++ FriendlyChatSwift/Plugins/MovieMessageCollectionViewCell.swift
@@ -12,7 +12,7 @@ import JSQMessagesViewController
 class MovieMessageCollectionViewCell: UICollectionViewCell {
     public static let cellReuseIdentifier = "MovieMessageCollectionViewCellIdentifier"
     public static let xibFileName = "MovieMessageCollectionViewCell"
-    
+    public static let rawHeight:CGFloat = 280
     
     @IBOutlet weak var messageBubbleContainerView: UIView!
     @IBOutlet weak var messageBubbleContainerWidthConstraint: NSLayoutConstraint!
@@ -20,22 +20,40 @@ class MovieMessageCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var messageBubbleTopLabel: UILabel!
     @IBOutlet weak var messageBubbleTopLabelHeightConstraint: NSLayoutConstraint!
     
-    @IBOutlet weak var textView: UITextView!
-    @IBOutlet weak var textViewAvatarHorizontalSpaceConstraint: NSLayoutConstraint!
-    @IBOutlet weak var textViewBottomVerticalSpaceConstraint:NSLayoutConstraint!
-    @IBOutlet weak var textViewMarginHorizontalSpaceConstraint: NSLayoutConstraint!
-    @IBOutlet weak var textViewTopVerticalSpaceConstraint: NSLayoutConstraint!
+    @IBOutlet weak var contentLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var contentTrailingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var movieImageView: UIImageView!
     
     @IBOutlet weak var leftAvatarSpacingConstraint: NSLayoutConstraint!
     @IBOutlet weak var rightAvatarSpacingConstraint: NSLayoutConstraint!
     @IBOutlet weak var leftAvatarImageView: UIImageView!
     @IBOutlet weak var rightAvatarImageView: UIImageView!
     
+    var imageRequest: URLRequest?
+    var movieModel: MoviePayload?
     
     var message: Message? {
         didSet {
-            if let movieModel = message?.parsedPayload as? MoviePayload {
-                textView.text = movieModel.title
+            movieModel = message?.parsedPayload as? MoviePayload
+            if let movieModel = movieModel {
+                let baseUrl = "http://image.tmdb.org/t/p/w500"
+                imageRequest = URLRequest(url: URL(string: baseUrl + movieModel.poster_path!)!)
+                
+                self.movieImageView.setImageWith(imageRequest!, placeholderImage: nil, success: { (imageRequest, imageResponse, image) in
+                    if imageResponse != nil {
+                        print("Image was NOT cached, fade in image")
+                        self.movieImageView.alpha = 0.0
+                        self.movieImageView.image = image
+                        UIView.animate(withDuration: 0.3, animations: { () -> Void in
+                            self.movieImageView.alpha = 1.0
+                        })
+                    } else {
+                        print("Image was cached so just update the image")
+                        self.movieImageView.image = image
+                    }
+                    }, failure: { (imageRequest, iresponse, error) in
+                        print("image fetch error \(error.localizedDescription)")
+                })
             }
         }
     }
@@ -56,10 +74,20 @@ class MovieMessageCollectionViewCell: UICollectionViewCell {
         
         leftAvatarSpacingConstraint.priority = isIncomingMessage ? 751 : 750
         rightAvatarSpacingConstraint.priority = isIncomingMessage ? 750 : 751
-        
+        contentLeadingConstraint.constant = isIncomingMessage ? 10: 5
+        contentTrailingConstraint.constant = isIncomingMessage ? 5: 10
+    }
+    
+    func handleTap(recognizer: UITapGestureRecognizer) {
+        print ("KEVINDEBUG I am touched \(self.movieModel?.title)")
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        movieImageView.layer.cornerRadius = 15.0
+        movieImageView.clipsToBounds = true
+        let tapGr = UITapGestureRecognizer(target: self, action: #selector(handleTap(recognizer:)))
+        movieImageView.addGestureRecognizer(tapGr)
+        movieImageView.isUserInteractionEnabled = true
     }
 }
